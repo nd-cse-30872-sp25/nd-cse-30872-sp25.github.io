4b7069af9702ee376821d7e7e2d5a2310b1f62b9
Bill <wtheisen@nd.edu>
2025-01-13 07:48:54 -0500

    making generate TAs better and adding pairings

--- scripts/generate_tas.py
+++ scripts/generate_tas.py
@@ -10,28 +10,31 @@ TAS      = [ta['github'] for ta in yaml.safe_load(open('../static/yaml/tas.yaml'
 STUDENTS = []
 
 for student in csv.DictReader(open('../static/csv/students.csv', 'r')):
-    STUDENTS.append(student['Netid'])
+    STUDENTS.append(student['SIS User ID'])
 
-CONFLICTS = dict(
-    (ta['github'], ta['conflicts'])
-    for ta in yaml.safe_load(open('../static/yaml/tas.yaml'))
-    if ta.get('conflicts')
-)
+for i in range(0, 12):
+    CONFLICTS = dict(
+        (ta['github'], ta['conflicts'])
+        for ta in yaml.safe_load(open('../static/yaml/tas.yaml'))
+        if ta.get('conflicts')
+    )
 
-HAS_CONFLICTS = True
+    HAS_CONFLICTS = True
 
+    while HAS_CONFLICTS:
+        random.shuffle(STUDENTS)
+        random.shuffle(TAS)
 
-while HAS_CONFLICTS:
-    random.shuffle(STUDENTS)
-    random.shuffle(TAS)
+        TAS           = TAS * (len(STUDENTS) // len(TAS) + 1)
+        MAPPING       = list(sorted(map(list, zip(STUDENTS, TAS))))
+        HAS_CONFLICTS = False
 
-    TAS           = TAS * (len(STUDENTS) // len(TAS) + 1)
-    MAPPING       = list(sorted(map(list, zip(STUDENTS, TAS))))
-    HAS_CONFLICTS = False
+        for ta, conflicts in CONFLICTS.items():
+            for conflict in conflicts:
+                if [conflict, ta] in MAPPING:
+                    HAS_CONFLICTS = True
 
-    for ta, conflicts in CONFLICTS.items():
-        for conflict in conflicts:
-            if [conflict, ta] in MAPPING:
-                HAS_CONFLICTS = True
-
-print(yaml.dump(MAPPING, default_flow_style=False))
+    # Write each mapping to a separate file
+    filename = f'../static/yaml/reading{i:02}.yaml'
+    with open(filename, 'w') as file:
+        yaml.dump(MAPPING, file, default_flow_style=False)
